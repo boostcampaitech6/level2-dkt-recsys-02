e92e891cd3fb5d7e2f526052aafa705361e0122b
ChangZero <changzero.kim@gmail.com>
2024-01-22 10:03:05 +0000

    Refactor: wandb logging & sweep.json

--- src/tabular/module/trainer.py
+++ src/tabular/module/trainer.py
@@ -52,8 +52,8 @@ def run(args, w_config):
     logger.info("TRAIN AUC : %.4f ACC : %.4f", auc, acc)
     
     # WandB Logging
-    # wandb.log(dict(valid_acc=acc,
-    #                valid_auc=auc))
+    wandb.log(dict(Val_Acc=acc,
+                   Val_AUC=auc))
     
     # INFERENCE
     logger.info("Preparing Test data ...")
